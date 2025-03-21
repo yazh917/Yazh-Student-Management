6d856208dc564a9f49c28845d0a828760fc433f4
yazh917 <ongyangzhi04@gmail.com>
2025-03-22 00:34:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,12 @@ $db="yzstudentmanagementsystem";
 $data = mysqli_connect($host, $user, $pass, $db);
 
 $sql="SELECT * FROM teacher ";
+$result = $conn->query($query);
+if (!$result) {
+    die("Query failed: " . $conn->error);
+}
 
 $result=mysqli_query($data,$sql);
+
 
 ?>
